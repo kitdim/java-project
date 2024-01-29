2fdcd70e20ea251f32b96bf49b20f7daaa177f47
kitdim <kitaykin9808@gmail.com>
2024-01-29 22:04:34 +0300

    add normalize of date and url

---
+++ app/src/main/java/hexlet/code/util/Normalizer.java
@@ -0,0 +1,22 @@
+package hexlet.code.util;
+
+import java.net.URL;
+import java.sql.Timestamp;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class Normalizer {
+    public static String getNormalizedURL(URL url) {
+        String protocol = url.getProtocol();
+        String symbols = "://";
+        String host = url.getHost();
+        String port = url.getPort() == -1 ? "" : ":" + url.getPort();
+
+        return protocol + symbols + host + port;
+    }
+    public static String getNormalizedTime(Timestamp time) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        return time.toLocalDateTime().format(formatter);
+    }
+}
