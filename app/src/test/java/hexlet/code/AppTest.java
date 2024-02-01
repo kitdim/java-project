fb7360bfa4e806bbf2e8c6b17ae0986efe5c4481
kitdim <kitaykin9808@gmail.com>
2024-02-02 02:06:12 +0300

    update test

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -3,33 +3,62 @@ package hexlet.code;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.util.StringJoiner;
 
 import hexlet.code.model.Url;
+import hexlet.code.repository.UrlCheckRepository;
 import hexlet.code.repository.UrlRepository;
 import hexlet.code.util.NamedRoutes;
 import io.javalin.http.HttpStatus;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import io.javalin.Javalin;
 import io.javalin.testtools.JavalinTest;
 
-public class AppTest {
-    Javalin app;
+public final class AppTest {
+    private static Javalin app;
+    private static MockWebServer mockServer;
+    private static String urlName;
+    private static final String HTML_PATH = "src/test/resources/index.html";
+
+    public static String getContentOfHtmlFile() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(HTML_PATH));
+        String lineOfFile = reader.readLine();
+        var result = new StringJoiner("\n");
+
+        while (lineOfFile != null) {
+            result.add(lineOfFile);
+            lineOfFile = reader.readLine();
+        }
+        return result.toString();
+    }
+
+    @BeforeAll
+    public static void beforeAll() throws IOException {
+        mockServer = new MockWebServer();
+        urlName = mockServer.url("/").toString();
+        var mockResponse = new MockResponse().setBody(getContentOfHtmlFile());
+        mockServer.enqueue(mockResponse);
+    }
 
     @BeforeEach
-    public final void setup() throws SQLException {
+    public void beforeEach() throws SQLException {
         app = App.getApp();
     }
 
-    @Test
-    public void testMainPage() {
-        JavalinTest.test(app, (server, client) -> {
-            var response = client.get(NamedRoutes.rootPath());
-            assertThat(response.code()).isEqualTo(HttpStatus.OK.getCode());
-        });
+    @AfterAll
+    public static void afterAll() throws IOException {
+        mockServer.shutdown();
     }
 
     @Test
@@ -43,7 +72,6 @@ public class AppTest {
     @Test
     public void testCreateUrl() throws SQLException {
         var url = Url.builder()
-                .id(1L)
                 .createdAt(new Timestamp(System.currentTimeMillis()))
                 .name("https://www.example1.com")
                 .build();
@@ -71,7 +99,6 @@ public class AppTest {
     @Test
     public void testShowUrl() throws SQLException {
         var url = Url.builder()
-                .id(1L)
                 .name("https://www.example1.com")
                 .createdAt(new Timestamp(System.currentTimeMillis()))
                 .build();
@@ -83,5 +110,27 @@ public class AppTest {
             response = client.get(NamedRoutes.urlPath(99L));
             assertThat(response.code()).isEqualTo(HttpStatus.NOT_FOUND.getCode());
         }));
+    }
+    @Test
+    public void testCheckUrl() throws SQLException {
+        var url = Url.builder()
+                .name(urlName)
+                .createdAt(new Timestamp(System.currentTimeMillis()))
+                .build();
+        UrlRepository.save(url);
+
+        JavalinTest.test(app, (server1, client) -> {
+            var response = client.post(NamedRoutes.urlCheckPath(url.getId()));
+            assertThat(response.code()).isEqualTo(HttpStatus.FOUND.getCode());
+
+            var urlCheck = UrlCheckRepository.find(url.getId()).orElse(null);
+            var title = urlCheck.getTitle();
+            var h1 = urlCheck.getH1();
+            var description = urlCheck.getDescription();
+
+            assertThat(title).isEqualTo("This is a title");
+            assertThat(h1).isEqualTo("This is a header");
+            assertThat(description).isEqualTo("This is a description");
+        });
     }
 }
