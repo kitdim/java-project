028bba31b9c744ce30a2364f1df07b5cd7a6f231
kitdim <kitaykin9808@gmail.com>
2024-02-02 02:07:37 +0300

    add the post method for creat the check

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -6,6 +6,7 @@ import gg.jte.ContentType;
 import gg.jte.TemplateEngine;
 import gg.jte.resolve.ResourceCodeResolver;
 import hexlet.code.controller.RootController;
+import hexlet.code.controller.UrlCheckController;
 import hexlet.code.controller.UrlController;
 import hexlet.code.repository.BaseRepository;
 import hexlet.code.util.NamedRoutes;
@@ -58,6 +59,7 @@ public final class App {
         app.get(NamedRoutes.urlsPath(), UrlController::index);
         app.get(NamedRoutes.urlPath("{id}"), UrlController::show);
         app.post(NamedRoutes.urlsPath(), UrlController::create);
+        app.post(NamedRoutes.urlCheckPath("{id}"), UrlCheckController::createCheck);
 
         return app;
     }
