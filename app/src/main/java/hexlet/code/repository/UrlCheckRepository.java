b2e04ea2819b4643ca8c948e9409a1897e3ce1c8
kitdim <kitaykin9808@gmail.com>
2024-02-02 02:09:37 +0300

    add repository

---
+++ app/src/main/java/hexlet/code/repository/UrlCheckRepository.java
@@ -0,0 +1,96 @@
+package hexlet.code.repository;
+
+import hexlet.code.model.UrlCheck;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static hexlet.code.repository.BaseRepository.dataSource;
+
+public class UrlCheckRepository {
+    public static void save(UrlCheck urlCheck) throws SQLException {
+        String sql = "INSERT INTO url_checks (url_id, status_code, h1, title, description, created_at) "
+                + "VALUES (?, ?, ?, ?, ?, ?)";
+        try (var conn = dataSource.getConnection();
+             var stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            stmt.setLong(1, urlCheck.getUrlId());
+            stmt.setInt(2, urlCheck.getStatusCode());
+            stmt.setString(3, urlCheck.getH1());
+            stmt.setString(4, urlCheck.getTitle());
+            stmt.setString(5, urlCheck.getDescription());
+            stmt.setTimestamp(6, urlCheck.getCreatedAt());
+            stmt.executeUpdate();
+
+            var generatedKeys = stmt.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                urlCheck.setId(generatedKeys.getLong(1));
+            } else {
+                throw new SQLException("DB have not returned an id after saving an entity");
+            }
+        }
+    }
+
+    public static Optional<List<UrlCheck>> getEntities(Long searchId) throws SQLException {
+        var sql = "SELECT * FROM url_checks where url_id = ?";
+        try (var conn = dataSource.getConnection();
+             var stmt = conn.prepareStatement(sql)) {
+            stmt.setLong(1, searchId);
+            var resultSet = stmt.executeQuery();
+            var result = new ArrayList<UrlCheck>();
+
+            while (resultSet.next()) {
+                var id = resultSet.getLong("id");
+                var urlId = resultSet.getLong("url_id");
+                var statusCode = resultSet.getInt("status_code");
+                var h1 = resultSet.getString("h1");
+                var title = resultSet.getString("title");
+                var description = resultSet.getString("description");
+                var createdAt = resultSet.getTimestamp("created_at");
+
+                var urlCheck = UrlCheck.builder()
+                        .id(id)
+                        .urlId(urlId)
+                        .statusCode(statusCode)
+                        .h1(h1)
+                        .title(title)
+                        .description(description)
+                        .createdAt(createdAt)
+                        .build();
+                result.add(urlCheck);
+            }
+            return Optional.of(result);
+        }
+    }
+
+    public static Optional<UrlCheck> find(Long id) throws SQLException {
+        var sql = "SELECT * FROM url_checks WHERE id = ?";
+        try (var conn = dataSource.getConnection();
+             var stmt = conn.prepareStatement(sql)) {
+            stmt.setLong(1, id);
+            var resultSet = stmt.executeQuery();
+            if (resultSet.next()) {
+                var urlId = resultSet.getLong("url_id");
+                var statusCode = resultSet.getInt("status_code");
+                var h1 = resultSet.getString("h1");
+                var title = resultSet.getString("title");
+                var description = resultSet.getString("description");
+                var createdAt = resultSet.getTimestamp("created_at");
+
+                var urlCheck = UrlCheck.builder()
+                        .id(id)
+                        .urlId(urlId)
+                        .statusCode(statusCode)
+                        .h1(h1)
+                        .title(title)
+                        .description(description)
+                        .createdAt(createdAt)
+                        .build();
+                return Optional.of(urlCheck);
+            }
+            return Optional.empty();
+        }
+    }
+}
