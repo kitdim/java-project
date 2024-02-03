2ab71921d041c3cc109867795653f46c8588fe61
kitdim <kitaykin9808@gmail.com>
2024-02-03 13:24:51 +0300

    update method getNormalizedURL, return to String.format

--- app/src/main/java/hexlet/code/util/Normalizer.java
+++ app/src/main/java/hexlet/code/util/Normalizer.java
@@ -5,14 +5,17 @@ import java.sql.Timestamp;
 import java.time.format.DateTimeFormatter;
 
 public class Normalizer {
-    public static String getNormalizedURL(URL url) {
-        String protocol = url.getProtocol();
-        String symbols = "://";
-        String host = url.getHost();
-        String port = url.getPort() == -1 ? "" : ":" + url.getPort();
-
-        return protocol + symbols + host + port;
+    public static String getNormalizedURL(URL parsedUrl) {
+        return String
+                .format(
+                        "%s://%s%s",
+                        parsedUrl.getProtocol(),
+                        parsedUrl.getHost(),
+                        parsedUrl.getPort() == -1 ? "" : ":" + parsedUrl.getPort()
+                )
+                .toLowerCase();
     }
+
     public static String getNormalizedTime(Timestamp time) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
         return time.toLocalDateTime().format(formatter);
