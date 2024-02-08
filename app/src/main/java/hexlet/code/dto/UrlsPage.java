4107e6f244a31ca5feaef84f24515be09b094467
kitdim <kitaykin9808@gmail.com>
2024-02-08 18:53:19 +0300

    remove unused the import, add constructor

--- app/src/main/java/hexlet/code/dto/UrlsPage.java
+++ app/src/main/java/hexlet/code/dto/UrlsPage.java
@@ -1,13 +1,19 @@
 package hexlet.code.dto;
 
+import hexlet.code.model.UrlCheck;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import hexlet.code.model.Url;
 
 import java.util.List;
+import java.util.Map;
 
 @Getter
 @AllArgsConstructor
 public class UrlsPage extends BasePage {
     private List<Url> urls;
+    private Map<Long, UrlCheck> checks;
+    public UrlsPage(List<Url> urls) {
+        this.urls = urls;
+    }
 }
